68c3c640d71bd064567fd1d3c7b9b96889546055
David <prozajik@gmail.com>
2018-09-29 18:02:24 +0200

    Fixed compilation

--- TransparentExtensionFilter/driver.c
+++ TransparentExtensionFilter/driver.c
@@ -22,6 +22,8 @@ Kernel mode
 #include <dontuse.h>
 #include <suppress.h>
 
+#include "driver.h"
+
 #pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
 
 //---------------------------------------------------------------------------
--- TransparentExtensionFilter/driver.h
+++ TransparentExtensionFilter/driver.h
@@ -1,1 +1,19 @@
 #pragma once
+
+NTSTATUS
+DriverEntry(
+	_In_ PDRIVER_OBJECT DriverObject,
+	_In_ PUNICODE_STRING RegistryPath
+);
+
+NTSTATUS
+NullUnload(
+	_In_ FLT_FILTER_UNLOAD_FLAGS Flags
+);
+
+NTSTATUS
+NullQueryTeardown(
+	_In_ PCFLT_RELATED_OBJECTS FltObjects,
+	_In_ FLT_INSTANCE_QUERY_TEARDOWN_FLAGS Flags
+);
+
